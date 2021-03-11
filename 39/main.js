4b53eeca314f4d7b2b69f1a268921e5390669cba
ZhekaTataru <74065931+ZhekaTataru@users.noreply.github.com>
2021-03-11 21:00:30 +0200

    Add files via upload

---
+++ 39/main.js
@@ -0,0 +1,33 @@
+
+// let a = prompt("Введите числло")
+// if(a>0 && a<5){
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+// let a =Number(prompt("Введите число"))
+// if(a == 0 || a == 2){
+//      alert(a + 7)
+// }
+// else{
+//     alert(a / 10)
+// }
+// let a =Number(prompt("Введите первое число" ))
+//     b =Number(prompt("Введите второе число"))
+//     if(a>2 && a<11 || b>3 || b == 3){
+//         alert(a+b)
+//     }
+// else{
+//     alert(a-b)
+// }
+
+
+
+
+
+
+
+
+
+
